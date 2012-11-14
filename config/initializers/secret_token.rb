b6bf1c59301c945754310e7a6d6bea994cdcc4b5
Pasha Rayan <pasha.rayan@gmail.com>
2012-11-14 13:28:09 +1100

    create initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hrapp::Application.config.secret_token = '20c95c1c40a3e334ab202016abb1df8ae67b9481b5698ba99ae71fb8e493e21f351a6c0102e7fbdb79b7b7e84d0285175226abdf792abcb6a4c9342e384497c6'
